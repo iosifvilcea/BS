7edd1fbf9bde34a0985c4196647531ce39b752b1
Iosif Vilcea <iosifvilcea@gmail.com>
2017-10-15 10:31:08 -0500

    Add Nav manager.

--- app/src/main/java/blankthings/bs/managers/NavigationManager.java
+++ app/src/main/java/blankthings/bs/managers/NavigationManager.java
@@ -1,11 +1,140 @@
 package blankthings.bs.managers;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import blankthings.bs.R;
+import blankthings.bs.ui.base.BaseFragment;
+import blankthings.bs.ui.base.BaseView;
+
 /**
  * Created by iosif on 10/13/17.
  */
 
 public class NavigationManager {
 
-    
+    private AppCompatActivity activity;
+    private BaseView baseView;
+
+    public NavigationManager(final AppCompatActivity mainActivity) {
+        activity = mainActivity;
+    }
+
+
+    private void ensureValidFragment(final Fragment fragment) {
+        if (!(fragment instanceof BaseFragment)) {
+            throw new IllegalArgumentException("Fragment is not an instance of BaseFragment.");
+        }
+    }
+
+
+    private void changeFragment(final Fragment fragment, final String tag, final boolean replace) {
+        ensureValidFragment(fragment);
+
+        final FragmentManager manager = activity.getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        if (replace) {
+            transaction.replace(R.id.content_main, fragment, tag);
+        } else {
+            transaction.add(R.id.content_main, fragment, tag);
+        }
+
+        transaction.addToBackStack(tag);
+        transaction.commit();
+    }
+
+
+    public boolean goBack() {
+        final FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        boolean backPressHandled = isHandledByFragment();
+
+        if (!backPressHandled) {
+            if (fragmentManager.getBackStackEntryCount() > 1) {
+                fragmentManager.popBackStackImmediate();
+                backPressHandled = true;
+            } else {
+                fragmentManager.popBackStackImmediate();
+                backPressHandled = false;
+            }
+        }
+
+        return backPressHandled;
+    }
+
+
+
+    public void goBackToFragment(final String tag) {
+        final FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        fragmentManager.popBackStack(tag, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    }
+
+
+    private boolean isHandledByFragment() {
+        final FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        boolean backPressHandled = false;
+
+        /** Notifies visible fragment of backpress. */
+        final Fragment fragment = fragmentManager.findFragmentById(R.id.content_main);
+        if (fragment != null && fragment instanceof OnBackPressedListener) {
+            backPressHandled = ((OnBackPressedListener) fragment).onBackPressed();
+        }
+
+        return backPressHandled;
+    }
+
+
+    @Override
+    public void startLoading() {
+        crossfadeLoading(true);
+    }
+
+
+    @Override
+    public void stopLoading() {
+        crossfadeLoading(false);
+    }
+
+
+    private void crossfadeLoading(boolean startLoading) {
+        int loadingAnimDuration = 400;
+        final float startAlpha, endingAlpha;
+        final int stopVisibility;
+        if (startLoading) {
+            startAlpha = 0f;
+            endingAlpha = 1f;
+            stopVisibility = View.VISIBLE;
+        } else {
+            startAlpha = 1f;
+            endingAlpha = 0f;
+            stopVisibility = View.GONE;
+        }
+
+        loadingView.setAlpha(startAlpha);
+        loadingView.setVisibility(View.VISIBLE);
+
+        loadingView.animate()
+                .alpha(endingAlpha)
+                .setDuration(loadingAnimDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        loadingView.setVisibility(stopVisibility);
+                    }
+                });
+    }
+
+
+    /**
+     * Notifies subscribers of back navigation.
+     */
+    public interface OnBackPressedListener {
+        boolean onBackPressed();
+    }
 
 }
